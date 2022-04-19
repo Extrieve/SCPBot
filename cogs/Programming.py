f6ead1654627fcf14d307630cc1a5149fe52f4dc
Nick <nicolas.cinera@gmail.com>
2022-04-19 13:30:29 -0400

    Better Date Format

--- cogs/Programming.py
+++ cogs/Programming.py
@@ -7,6 +7,7 @@ import os
 import sys
 from datetime import datetime
 import pytz
+import dateutil.parser
 
 
 class Programming(commands.Cog):
@@ -21,61 +22,18 @@ class Programming(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # # contest information
-    # @commands.command(name='contest', aliases=['contestinfo'])
-    # def ztoest(value):
-    #     d, t = value.split('T')
-    #     l = []
-    #     l2 = []
-    #     l = t.split(':')
-    #     l2 = d.split('-')
-    #     i2 = int(l2[2])
-    #     i = int(l[0])
-    #     i -= 4
-    #     if i < 0:
-    #         i += 24
-    #         i2 -= 1
-    #         l[0] = str(i)
-    #         l2[2] = str(i2)
-    #     else:
-    #         l[0] = str(i)
+    def time_convert(self, convert, fmt):
+        """Converts time to a different format."""
+        fmt = dateutil.parser.parse(convert)
+        # convert zulu time to eastern standard time
+        fmt = fmt.astimezone(pytz.timezone('US/Eastern'))
+        # convert format to d-m-Y H:M:S
+        fmt = fmt.strftime('%d-%m-%Y %H:%M:%S')
 
-    #     date = f'{l2[1]}-{l2[2]}-{l2[0]}'
-    #     time = f'{l[0]}:{l[1]} EST'
-    #     return date, time
+        return fmt.split()
 
-    def time_convert(self, convert):
-        est = pytz.timezone('US/Eastern')
-        utc = pytz.utc
-        # 2022-05-08T12:00:00.000Z format
-        fmt = '%Y-%m-%dT%H:%M:%S.000Z'
-
-        winter = datetime(2022, 1, 24, 18, 0, 0, tzinfo=utc)
-        summer = datetime(2022, 7, 24, 18, 0, 0, tzinfo=utc)
-
-        try:
-            start_time = datetime.strptime(convert, fmt)
-        except ValueError:
-            start_time = datetime.strptime(convert.replace(' UTC', ''), '%Y-%m-%d %H:%M:%S')
-        start_time = start_time.astimezone(est)
-
-        date, time = start_time.strftime('%Y-%m-%d'), str(start_time).split(' ')[1]
-        difference = time.split('-')[-1]
-        difference = int(difference.split(':')[0])
-
-        new_hour = int(time.split(':')[0]) - difference
-
-        time = time.split(':')
-        time[0] = str(new_hour)
-        time = ':'.join(time)
-
-        index = time.index('-') if '-' in time else time.index('+')
-        time = time[:index]
-
-        return date, time
 
     # contest information
-
     @commands.command(name='contest', aliases=['contestinfo'])
     async def contest(self, ctx, *, site=''):
         """
@@ -106,15 +64,15 @@ class Programming(commands.Cog):
         data = r.json()
         contests = []
         for entry in data:
-            start_d, start_t = self.time_convert(entry['start_time'])
-            end_d, end_t = self.time_convert(entry['end_time'])
+            # print(f"HERE HERE HERE: {'T' in entry['start_time']}")
+            start_d, start_t = self.time_convert(entry['start_time'], self.available_contests[site][1])
+            end_d, end_t = self.time_convert(entry['end_time'], self.available_contests[site][1])
             contests.append(
-                f'{entry["name"]} - Day: {start_d} - Start: {start_t} - End: {end_t} - {entry["url"]}')
+                f'{entry["name"]} - Date: {start_d} - Start: {start_t} - End: {end_t} - {entry["url"]}')
 
         # embed the contest data
-        embed = discord.Embed(title=f'{" ".join([item.capitalize() for item in site.split("_")])}', description='\n'.join(
-            contests), color=0x00ff00)
-        image = self.available_contests[site]
+        embed = discord.Embed(title=f'{" ".join([item.capitalize() for item in site.split("_")])}', description='\n'.join(contests), color=0x00ff00)
+        image = self.available_contests[site][0]
         embed.set_thumbnail(url=image)
         await ctx.send(embed=embed)
 
--- setup.py
+++ setup.py
@@ -1,15 +1,14 @@
 import os
 
-available_contests = {'codeforces': 'https://play-lh.googleusercontent.com/EkSlLWf2-04k5Y5F_MDLqoXPdo0TyZX3zKdCfsEUDqVB7INUypTOd6AVmkE_X7ej3JuR',
-                      'top_coder': 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/tdenoarg7lu2emnoyu7c',
-                      'at_coder': 'https://img.atcoder.jp/assets/atcoder.png',
-                      'cs_academy': 'https://students.ieee.org/wp-content/uploads/2018/08/csacademy_sponsor.png',
-                      'code_chef': 'https://pbs.twimg.com/profile_images/1477930785537605633/ROTVNVz7_400x400.jpg',
-                      'hacker_rank': 'https://upload.wikimedia.org/wikipedia/commons/4/40/HackerRank_Icon-1000px.png',
-                      'hacker_earth': 'https://upload.wikimedia.org/wikipedia/commons/e/e8/HackerEarth_logo.png',
-                      'kick_start': 'https://bookassist.org/wp-content/uploads/2021/07/google-logo.jpg',
-                      'leet_code': 'https://leetcode.com/static/images/LeetCode_logo_rvs.png',
-                      'toph': 'https://help.toph.co/emblems/platform.2246769acdc0086a24ee5a72689be221a0d7e8bab8897faac42ff0664be37a8e.png'}
+available_contests = {'codeforces': ('https://play-lh.googleusercontent.com/EkSlLWf2-04k5Y5F_MDLqoXPdo0TyZX3zKdCfsEUDqVB7INUypTOd6AVmkE_X7ej3JuR', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'top_coder': ('https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/tdenoarg7lu2emnoyu7c', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'at_coder': ('https://img.atcoder.jp/assets/atcoder.png', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'cs_academy': ('https://students.ieee.org/wp-content/uploads/2018/08/csacademy_sponsor.png', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'code_chef': ('https://pbs.twimg.com/profile_images/1477930785537605633/ROTVNVz7_400x400.jpg', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'hacker_rank': ('https://upload.wikimedia.org/wikipedia/commons/4/40/HackerRank_Icon-1000px.png', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'hacker_earth': ('https://upload.wikimedia.org/wikipedia/commons/e/e8/HackerEarth_logo.png', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'kick_start': ('https://bookassist.org/wp-content/uploads/2021/07/google-logo.jpg', '%Y-%m-%dT%H:%M:%S.%LZ'),
+                      'leet_code': ('https://leetcode.com/static/images/LeetCode_logo_rvs.png', '%Y-%m-%dT%H:%M:%S.%LZ')}
 
 # token = "place your token here"
 cwd = os.getcwd()
